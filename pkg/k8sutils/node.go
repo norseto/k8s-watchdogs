c394f71f75a2bfb56e6577f982422898cb2f4b9a
Norihiro Seto <norihiros@acm.org>
2024-01-05 11:57:19 +0900

    [WIP] Node schedule check

--- pkg/k8sutils/node.go
+++ pkg/k8sutils/node.go
@@ -22,3 +22,41 @@ func GetUntaintedNodes(ctx context.Context, c kubernetes.Interface) ([]v1.Node, 
 	}
 	return nodes, nil
 }
+
+// IsScheduleable checks whether a node is schedulable for a given pod specification.
+func IsScheduleable(node *v1.Node, spec *v1.PodSpec) bool {
+	return tolerationsTolerateTaints(spec.Tolerations, node.Spec.Taints)
+}
+
+// Function that checks if Tolerations can tolerate the given Taints
+func tolerationsTolerateTaints(tolerations []v1.Toleration, taints []v1.Taint) bool {
+	for _, taint := range taints {
+		taintNotTolerated := true
+		for _, toleration := range tolerations {
+			if toleratesTaint(&toleration, &taint) {
+				taintNotTolerated = false
+				break
+			}
+		}
+		if taintNotTolerated {
+			return false
+		}
+	}
+	return true
+}
+
+// Function that checks if a Taint is tolerated by a Toleration
+func toleratesTaint(toleration *v1.Toleration, taint *v1.Taint) bool {
+	if len(toleration.Effect) > 0 && toleration.Effect != taint.Effect {
+		return false
+	}
+	if len(toleration.Key) > 0 && toleration.Key != taint.Key {
+		return false
+	}
+	if len(toleration.Operator) > 0 && toleration.Operator != v1.TolerationOpExists {
+		equalityBased := (toleration.Operator == v1.TolerationOpEqual && toleration.Value == taint.Value)
+		existsBased := toleration.Operator == v1.TolerationOpExists
+		return equalityBased || existsBased
+	}
+	return true
+}
