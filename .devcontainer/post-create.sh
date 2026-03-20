eeceae35c34db082eb104669793c899cf5e3677a
Norihiro Seto <norihiros@acm.org>
2026-03-21 00:22:47 +0900

    add volumes for ACP

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 
-sudo chown -R $(id -u):$(id -g) $HOME/.codex $HOME/.claude \
+sudo chown -R $(id -u):$(id -g) $HOME/.codex $HOME/.claude $HOME/.cursor $HOME/.copilot \
   /home/vscode/.aws /home/vscode/.kube \
   /home/vscode/.gocache /home/vscode/.gomodcache /go
